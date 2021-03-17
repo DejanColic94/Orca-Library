368721f15d969edad54cb974c33a6a1dc366f246
Dejan Colic <dejan.colic99@gmail.com>
2021-03-17 11:59:45 +0100

    server and client

---
+++ OrcaLibraryServer/src/com/orca/main/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.orca.main;
+
+import com.orca.view.FrmServer;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author DCX
+ */
+public class Main {
+    public static void main(String[] args) {
+        JFrame server = new FrmServer();
+        server.setVisible(true);
+        
+    }
+}
