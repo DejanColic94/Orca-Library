a97ffd543985a4a2cccbe5f0308bfef7ab99bcaa
Dejan Colic <dejan.colic99@gmail.com>
2021-09-15 19:15:12 +0200

    System operations defined

---
+++ OrcaLibraryServer/src/system_operations/clan/SONadjiClanove.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package system_operations.clan;
+
+import system_operations.GeneralizedSO;
+
+/**
+ *
+ * @author DCX
+ */
+public class SONadjiClanove extends GeneralizedSO{
+    
+}
---
+++ OrcaLibraryServer/src/system_operations/knjiga/SOIzbrisiKnjigu.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package system_operations.knjiga;
+
+import system_operations.GeneralizedSO;
+
+/**
+ *
+ * @author DCX
+ */
+public class SOIzbrisiKnjigu extends GeneralizedSO{
+    
+}
